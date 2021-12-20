e907b0b8e078d6bfa3e90641e1b844ab8d1e22aa
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-20 14:51:18 +0530

    Update TecAdminSeleniumTest.java

--- TecAdminSeleniumTest.java
+++ TecAdminSeleniumTest.java
@@ -1,5 +1,4 @@
-mport java.io.IOException;
- 
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
